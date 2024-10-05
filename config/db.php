297aa455980a6062399890694bc9d8ff33a0e332
Antônio Negreiros <antonio.negreiros01@gmail.com>
2024-10-04 21:58:54 -0300

    revisao04_10

--- config/db.php
+++ config/db.php
@@ -1,13 +1,37 @@
-<?php
+<?php 
 $host = "localhost";
 $db_name = "aulaphp";
 $db_user = "root";
 $db_password = "";
 
+$token = "abacaxi"; // salt
+
 try {
-    $pdo = new PDO("mysql:host=$host;dbname=$db_name",$db_user, $db_password);
-    echo "Conexão bem sucessida, By Antônio";
+    $pdo = new PDO("mysql:host=$host;dbname=$db_name", $db_user, $db_password);
+    // echo "conexão bem sucedida";
 } catch (PDOException $e){
     echo $e->getMessage();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 ?>
--- header.php
+++ header.php
@@ -1,7 +1,7 @@
 <html>
 
 <head>
-    <title>Titulo da página</title>
+    <title>Página Inicial</title>
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="assets/css/styles.css">
